8012beb3c847353afdf6cdbb749c0d5bbc3a4c5d
sohagroy7223 <freelancersohagroy@gmail.com>
2026-08-08 13:25:43 +0600

    addad zap shift client first commit ,now this vite project add to tailwind css , daisy ui, react router and some pages navbar and foooter

---
+++ src/routers/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
