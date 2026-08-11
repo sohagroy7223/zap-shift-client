7c24d3f689d7e4875cd5f98dcf3955baf9105568
sohagroy7223 <freelancersohagroy@gmail.com>
2026-08-11 18:32:57 +0600

    addad FAQ this section

--- src/pages/Home/FAQ/FAQ.jsx
+++ src/pages/Home/FAQ/FAQ.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FAQ = () => {
   return (
-    <div>
+    <div className="space-y-3">
       <div className="md:w-3xl mx-auto text-center">
         <h3 className=" md:text-2xl text-lg text-center font-bold px-3">
           Frequently Asked Question (FAQ)
@@ -12,6 +12,56 @@ const FAQ = () => {
           Pro. Achieve proper alignment, reduce pain, and strengthen your body
           with ease!
         </p>
+      </div>
+      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" defaultChecked />
+        <div className="collapse-title font-semibold">
+          How does this posture corrector work?
+        </div>
+        <div className="collapse-content text-sm">
+          A posture corrector works by providing support and gentle alignment to
+          your shoulders, back, and spine, encouraging you to maintain proper
+          posture throughout the day. Here’s how it typically functions: A
+          posture corrector works by providing support and gentle alignment to
+          your shoulders.
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title font-semibold">
+          Is it suitable for all ages and body types?
+        </div>
+        <div className="collapse-content text-sm">
+          Click on "Forgot Password" on the login page and follow the
+          instructions sent to your email.
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title font-semibold">
+          Does it really help with back pain and posture improvement?
+        </div>
+        <div className="collapse-content text-sm">
+          Go to "My Account" settings and select "Edit Profile" to make changes.
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title font-semibold">
+          Does it have smart features like vibration alerts?
+        </div>
+        <div className="collapse-content text-sm">
+          Go to "My Account" settings and select "Edit Profile" to make changes.
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title font-semibold">
+          How will I be notified when the product is back in stock?
+        </div>
+        <div className="collapse-content text-sm">
+          Go to "My Account" settings and select "Edit Profile" to make changes.
+        </div>
       </div>
     </div>
   );
