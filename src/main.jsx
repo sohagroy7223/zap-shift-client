880ad920028518364b12ee905dd172c0bc390988
sohagroy7223 <freelancersohagroy@gmail.com>
2026-08-19 22:45:37 +0600

    get data from data base by the ten stack

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./routers/Router.jsx";
 import AuthProvider from "./Context/AuthProvider.jsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
--- src/pages/Dashboard/My_Parcel/MyParcel.jsx
+++ src/pages/Dashboard/My_Parcel/MyParcel.jsx
@@ -1,9 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const MyParcel = () => {
+  const { user } = useAuth();
+
+  const axiosInstance = useAxiosSecure();
+
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["myParcel", user?.email],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/parcels?email=${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(parcels);
   return (
     <div className="px-5">
-      <h3>my parcel component</h3>
+      <h3>my parcel component {parcels.length} </h3>
     </div>
   );
 };
