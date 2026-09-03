b51446e4180e04d57480fbd660010069ba475dfe
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-03 21:31:49 +0600

    addad payment option if user payment success fully status shoe in client side paid else show pay

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  const axiosSecure = useAxiosSecure();
+  console.log(sessionId);
+
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+        });
+    }
+  }, [sessionId, axiosSecure]);
+
   return (
     <div>
       <div className=" h-screen text-center mx-auto flex flex-col items-center justify-center">
