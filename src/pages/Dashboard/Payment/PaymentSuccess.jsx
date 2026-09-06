1ab45b667c69abb7fd55c32e7db44485063d1dc5
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-06 10:44:35 +0600

    finaly i fixt the bug that insertedId only 1 count in database

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useRef } from "react";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
   const [paymentInfo, setPaymentInfo] = useState({});
   const sessionId = searchParams.get("session_id");
   const axiosSecure = useAxiosSecure();
+  const called = useRef(false);
   // console.log(sessionId);
 
   useEffect(() => {
-    if (sessionId) {
+    if (sessionId && !called.current) {
+      called.current = true;
+
       axiosSecure
         .patch(`/payment-success?session_id=${sessionId}`)
         .then((res) => {
+          console.log(res.data);
           setPaymentInfo({
             transactionId: res.data.transactionId,
             trackingId: res.data.trackingId,
@@ -25,18 +30,17 @@ const PaymentSuccess = () => {
   return (
     <div>
       <div className=" h-screen text-center mx-auto flex flex-col items-center justify-center">
+        <h3 className=" text-3xl font-bold">Payment successfully pay</h3>
+        <br />
         <p>
           Your transaction id :
           <span className="font-bold">{paymentInfo.transactionId}</span>
         </p>
         <p>
           your Tracking id :
-          <span className="font-bold" s>
-            {paymentInfo.trackingId}
-          </span>
+          <span className="font-bold">{paymentInfo.trackingId}</span>
         </p>
-        <h3 className=" text-3xl font-bold">Payment successfully pay</h3>
-
+        <br />
         <img className="rounded-full w-30 h-30" src="/success.png" alt="" />
       </div>
     </div>
