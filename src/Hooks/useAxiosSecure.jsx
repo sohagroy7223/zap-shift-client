296d02e358bd0e655da396c200e93111026992c4
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-07 22:08:42 +0600

    addad axiosInterceptor and send the user access token for verify user

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,11 +1,21 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user } = useAuth();
+  console.log(user);
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+  }, []);
+
   return axiosSecure;
 };
 
