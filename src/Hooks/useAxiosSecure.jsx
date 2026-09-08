20cbb783bf2e02e72bd12774e832e7a1414f544f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-08 11:56:04 +0600

    addad intersecptor request responce and handel error if error status 401 or 403 id true user logout and go t on login page

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,20 +1,44 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
-  console.log(user);
+  const { user, signOutUser } = useAuth();
+  const navigate = useNavigate();
+  // console.log(user);
   useEffect(() => {
-    axiosSecure.interceptors.request.use((config) => {
+    // request interceptor
+    const resInterceptor = axiosSecure.interceptors.request.use((config) => {
       config.headers.Authorization = `Bearer ${user?.accessToken}`;
       return config;
     });
-  }, []);
+    // response interceptor
+    const reqInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+        const statusCode = error.status;
+        if (statusCode === 401 || 403) {
+          signOutUser().then(() => {
+            navigate("/login");
+          });
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(resInterceptor);
+    };
+  }, [user]);
 
   return axiosSecure;
 };
