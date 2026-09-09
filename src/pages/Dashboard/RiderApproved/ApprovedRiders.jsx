8125bc4a3313b09bb2d9e589f40a091490da08d2
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-09 17:57:20 +0600

    addad update function if user approved to role a rider

--- src/pages/Dashboard/RiderApproved/ApprovedRiders.jsx
+++ src/pages/Dashboard/RiderApproved/ApprovedRiders.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { FaTrashAlt, FaUserCheck } from "react-icons/fa";
 import { IoPersonRemove } from "react-icons/io5";
+import Swal from "sweetalert2";
 
 const ApprovedRiders = () => {
   const axiosSecure = useAxiosSecure();
@@ -14,7 +15,30 @@ const ApprovedRiders = () => {
       return res.data;
     },
   });
-  console.log(riders);
+
+  const handelUpdateRiders = (id, status) => {
+    const updateInfo = { status: `${status}` };
+    axiosSecure.patch(`/riders/${id}`, updateInfo).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Rider status set to ${status}`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    });
+  };
+
+  const handelApprovedRider = (id) => {
+    handelUpdateRiders(id, "approved");
+  };
+  const handelRejectsRider = (id) => {
+    handelUpdateRiders(id, "rejected");
+  };
+
   return (
     <div>
       <h3>Rider pending approved page : {riders.length}</h3>
@@ -34,17 +58,27 @@ const ApprovedRiders = () => {
           </thead>
           <tbody>
             {riders.map((rider, index) => (
-              <tr>
+              <tr key={rider._id}>
                 <th>{index + 1}</th>
                 <td>{rider.name}</td>
                 <td>{rider.email}</td>
                 <td>{rider.district}</td>
-                <td>{rider.status}</td>
+                <td
+                  className={`${rider.status === "approved" ? "text-green-700" : "text-yellow-500"}`}
+                >
+                  {rider.status}
+                </td>
                 <td>
-                  <button className="btn btn-sm">
+                  <button
+                    onClick={() => handelApprovedRider(rider._id)}
+                    className="btn btn-sm"
+                  >
                     <FaUserCheck />
                   </button>
-                  <button className="btn btn-sm">
+                  <button
+                    onClick={() => handelRejectsRider(rider._id)}
+                    className="btn btn-sm"
+                  >
                     <IoPersonRemove />
                   </button>
                   <button className="btn btn-sm">
