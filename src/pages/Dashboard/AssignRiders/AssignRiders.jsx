ce24d8c25b4b110f485f717a430904c34961e9ac
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-15 11:34:29 +0600

    addad assign rider page

--- src/pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -13,9 +13,44 @@ const AssignRiders = () => {
       return res.data;
     },
   });
+  console.log(parcels);
   return (
     <div>
       <h3>Assign riders : {parcels.length}</h3>
+      {
+        // parcels.map(parcel=>)
+      }
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th>Created at</th>
+              <th>Pickup District at</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>$ {parcel.cost}</td>
+                <td>{new Date(parcel.createdAt).toLocaleDateString()}</td>
+                <td>{parcel.senderDistrict}</td>
+                <td>
+                  <button className="btn hover:btn-primary btn-sm text-secondary">
+                    Assign Rider
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
