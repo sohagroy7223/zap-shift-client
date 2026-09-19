84a531de9172aa741e0c2cd4f209c6fe779809b1
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-20 04:42:09 +0600

    get deliverd data and show dada in complited delivers

--- src/pages/Dashboard/Completed-delivers/CompletedDelivers.jsx
+++ src/pages/Dashboard/Completed-delivers/CompletedDelivers.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const CompletedDelivers = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [], refetch } = useQuery({
+    queryKey: ["parcels", user.email, "delivery_assign"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/parcels/rider?riderEmail=${user.email}&deliveryStatus=parcel-delivered`,
+      );
+      //   console.log(res.data);
+      return res.data;
+    },
+  });
+  console.log(parcels);
   return (
     <div>
-      <h3>this is completed delivery page</h3>
+      <h3>Completed delivers: {parcels.length}</h3>
+
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Cost</th>
+              <th>Created at</th>
+              <th>Pickup District</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>$ {parcel.cost}</td>
+                <td>{new Date(parcel.createdAt).toLocaleDateString()}</td>
+                <td>{parcel.senderDistrict}</td>
+                <td>
+                  <button className="btn hover:btn-primary btn-sm text-secondary">
+                    Find Rider
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
