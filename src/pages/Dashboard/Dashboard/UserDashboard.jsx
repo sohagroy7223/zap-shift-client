c43880c9b6c22278845f7950ed697814e6560798
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-25 13:50:05 +0600

    addad user dashboard this page

--- src/pages/Dashboard/Dashboard/UserDashboard.jsx
+++ src/pages/Dashboard/Dashboard/UserDashboard.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../Hooks/useAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
 
 const UserDashboard = () => {
   const { user } = useAuth();
@@ -14,11 +16,79 @@ const UserDashboard = () => {
       return res.data;
     },
   });
-  console.log(users);
+
+  const colorMap = {
+    "pending-pickup": "#0088FE",
+    "delivery-assigned": "#FFBB28",
+    "in-transit": "#00C49F",
+  };
+
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["parcels-delivery-status"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels/delivery-status/status`);
+      return res.data;
+    },
+  });
+  const getPiChartData = (data) => {
+    return data.map((item) => {
+      return {
+        name: item.status,
+        value: item.count,
+        fill: colorMap[item.status] || "#8884D8",
+      };
+    });
+  };
 
   return (
     <div>
       <h3>user Dashboard :{users.length}</h3>
+
+      <div className="stats stats-vertical lg:stats-horizontal shadow">
+        <div className="stat">
+          <div className="stat-title">Total send Parcels</div>
+          <div className="stat-value">{users.length}</div>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="stats shadow">
+        {parcels.map((parcel) => (
+          <div key={parcel._id} className="stat place-items-center">
+            <div className="stat-title">{parcel.status}</div>
+            <div className="stat-value">{parcel.count}</div>
+          </div>
+        ))}
+      </div>
+
+      {/* pi chart */}
+      <div className="w-full h-100">
+        <PieChart
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            maxHeight: "80vh",
+            aspectRatio: 2,
+          }}
+          responsive
+        >
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={getPiChartData(parcels)}
+            cx="50%"
+            cy="100%"
+            outerRadius="120%"
+            label
+            isAnimationActive={true}
+          />
+          <Tooltip></Tooltip>
+          <Legend></Legend>
+          <RechartsDevtools />
+        </PieChart>
+      </div>
     </div>
   );
 };
