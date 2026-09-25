05c4fcb531ba3c57c7e3666049983ad7a6922aef
sohagroy7223 <freelancersohagroy@gmail.com>
2026-09-25 13:09:46 +0600

    get all parcels detais

--- src/pages/Dashboard/Dashboard/UserDashboard.jsx
+++ src/pages/Dashboard/Dashboard/UserDashboard.jsx
@@ -1,9 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const UserDashboard = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["parcels-delivery-status", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data;
+    },
+  });
+  console.log(users);
+
   return (
     <div>
-      <h3>user Dashboard</h3>
+      <h3>user Dashboard :{users.length}</h3>
     </div>
   );
 };
